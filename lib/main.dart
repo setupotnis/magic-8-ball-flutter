1a0c4773a51f7b98dd4632b7b0fa5ffead91ae4d
Setu Potnis <setupotnis@gmail.com>
2020-03-10 18:30:26 -0400

    made stateful widget and added button for magic 8 ball
    also made onPressed functin to make app a bit interactive

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: BallPage(),
+        home: Ball(),
       ),
     );
 
-class BallPage extends StatelessWidget {
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          title: Text('Ask Me Anything'),
-          backgroundColor: Colors.blue[800],
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[800],
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Image.asset('images/ball1.png'),
+              onPressed: () {
+                print('I got clicked!');
+              },
+            ),
+          ],
         ),
-        body: Container(),
       ),
     );
   }
