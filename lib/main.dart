d6a14e3a6214b2801e0fa87b7b2288b6b691387a
Setu Potnis <setupotnis@gmail.com>
2020-03-10 18:18:53 -0400

    added stateless widget and basic UI features

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[800],
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
